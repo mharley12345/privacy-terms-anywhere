b9ab6d5b3e1f9775250c1d71d4fbfb3b460f7137
Mike Harley <mharley12345@gmail.com>
2020-02-10 11:52:35 -0800

    .

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from django.conf.urls import url,include
 from rest_framework.authtoken import views
-
+#from .views import answers
 
 
 
@@ -9,7 +9,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('login/', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
-
+ #   url('answers', answers),
 
    
 ]
