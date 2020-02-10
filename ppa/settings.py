1b5d2efebbbd063771154592f3c4fb4a8d154e02
Mike Harley <mharley12345@gmail.com>
2020-02-10 12:29:53 -0800

    .

--- ppa/settings.py
+++ ppa/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['privacy-aw.herokuapp.com']
+ALLOWED_HOSTS = ['privacy-aw.herokuapp.com','localhost']
 
 
 # Application definition
